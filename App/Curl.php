2e7d579e9be5e1bd334916ca7f741df1e1792b22
Tony <dima.dima.maksimov1998@mail.ru>
2018-01-03 21:18:24 +0800

    Update class Curl

--- App/Curl.php
+++ App/Curl.php
@@ -41,60 +41,61 @@ class Curl
 		curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, $value);
 	}
 
-	public function request($url){
+	public function request(string $url)
+	{
 		curl_setopt($this->ch, CURLOPT_URL, $this->make_url($url));
 		$data = curl_exec($this->ch);
 		return $this->process_result($data);
 	}
 
-	public function config_load(){
+	public function config_load()
+	{
 
 	}
 
-	public function config_save($file){
+	public function config_save($file)
+	{
 
 	}
 
-	private function make_url($url){
-		if($url[0] != '/')
+	private function make_url(string $url)
+	{
+		if ('/' != $url[0]) {
 			$url = '/' . $url;
-
+		}
 		return $this->host . $url;
 	}
 
-	private function process_result($data){
+	private function process_result(string $data)
+	{
 		$p_n = "\n";
 		$p_rn = "\r\n";
-
 		$h_end_n = strpos($data, $p_n . $p_n);    // int - false
 		$h_end_rn = strpos($data, $p_rn . $p_rn); // int - false
-
 		$start = $h_end_n; // h_end_n int
 		$p = $p_n;		 // \n
 
-		if($h_end_n === false || $h_end_rn < $h_end_n){
+		if ($h_end_n === false || $h_end_rn < $h_end_n) {
 			$start = $h_end_rn;
 			$p = $p_rn;
 		}
 
 		$headers_part = substr($data, 0, $start);
 		$body_part = substr($data, $start + strlen($p) * 2);
-
 		$lines = explode($p,$headers_part);
-		$headers = array();
+		$headers = [];
+		$headers['start'] = array_shift($lines);
 
-		$headers['start'] = $lines[0];
-
-		for($i = 1; $i < count($lines); $i++){
-			$del_pos = strpos($lines[$i], ':');
-			$name = substr($lines[$i], 0, $del_pos);
-			$value = substr($lines[$i], $del_pos + 2);
+		foreach ($lines as $v) {
+			$del_pos = strpos($v, ':');
+			$name = substr($v, 0, $del_pos);
+			$value = substr($v, $del_pos + 2);
 			$headers[$name] = $value;
 		}
 
-		return array(
+		return [
 			'headers' => $headers,
 			'html' => $body_part
-		);
+		];
 	}
 }
